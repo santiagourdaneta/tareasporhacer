0f53069ae3fd20d329b059864a302414b15b1ec6
Luis Villalba <luisvillalba244@gmail.com>
2017-09-03 23:17:15 -0500

    Trabajo solo con vista crear

---
+++ resources/views/create.blade.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="newTask" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">
+					<span>&times;</span>
+				</button>
+				<h1 class="text-center">Nueva Tarea</h1>
+			</div>
+			<div class="modal-body">
+				Incoming...
+			</div>
+			<div class="modal-footer">
+				<input type="submit" class="btn btn-success center-block" value="Crear nueva">
+			</div>
+		</div>
+	</div>
+</div>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,11 @@
 @extends('layouts.app')
 @section('content')
-
-<h1 class="page-header text-center">Bienvenido</h1>
-
 <div id="crud" class="row">
 	<div class="col-xs-12">
 		<h3 class="page-header">Muestra de CRUD</h3>
 	</div>
 	<div class="col-sm-7">
-		<a href="" class="btn btn-success pull-right">Nueva tarea</a>
+		<a href="" class="btn btn-success pull-right" data-toggle="modal" data-target="#newTask">Nueva tarea</a>
 		<table class="table table-hover table-striped">
 			<thead>
 				<tr>
@@ -26,14 +23,17 @@
 					<td>@{{ keep.keep }}</td>
 					<td>@{{ keep.created_at }}</td>
 					<td width="10px">
-					<a href="#" class="btn btn-warning btn-sm">Editar</a>
+						<a href="#" class="btn btn-warning btn-sm">Editar</a>
 					</td>
 					<td width="10px">
-					<a href="#" class="btn btn-danger btn-sm" v-on:click.prevent="deleteKeep(keep)">Eliminar</a>
+						<a href="#" class="btn btn-danger btn-sm" v-on:click.prevent="deleteKeep(keep)">Eliminar</a>
 					</td>
 				</tr>
 			</tbody>
 		</table>
+
+		@include('create')
+
 	</div>
 	<div class="col-sm-5">
 		<pre>
